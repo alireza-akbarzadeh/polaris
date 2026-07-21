66b68c8bb0d8df14a40af3de0e85d000c3901cd3
Alireza Akbarzadeh <82927248+alireza-akbarzadeh@users.noreply.github.com>
2026-07-21 21:57:50 +0330

    Update package dependencies in package.json and package-lock.json, refactor Providers component to streamline imports, enhance AuthLoadingView for improved UI, and implement URL scraping functionality in Inngest functions.

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ClerkProvider, SignIn, useAuth, UserButton } from "@clerk/nextjs";
+import { ClerkProvider, useAuth, UserButton } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import {
   Authenticated,
@@ -9,10 +9,10 @@ import {
   Unauthenticated,
 } from "convex/react";
 
+import { AuthLoadingView } from "@/features/auth/components/auth-loading-view";
+import { UnauthenticatedView } from "@/features/auth/components/unathenticated-view";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import ThemeProvider from "./theme-provider";
-import { UnauthenticatedView } from "@/features/auth/components/unathenticated-view";
-import { AuthLoadingView } from "@/features/auth/components/auth-loading-view";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
@@ -38,7 +38,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
             <UnauthenticatedView />
           </Unauthenticated>
           <AuthLoading>
-            <AuthLoadingView/>
+            <AuthLoadingView />
           </AuthLoading>
         </ThemeProvider>
       </ConvexProviderWithClerk>
--- src/inngest/fucntions.ts
+++ src/inngest/fucntions.ts
@@ -1,6 +1,8 @@
-import { inngest } from "./client";
-import { generateText } from "ai";
+import firecrawl from "@/lib/firecrawl";
+import { URL_REGEX } from "@/lib/regex";
 import { google } from "@ai-sdk/google";
+import { generateText } from "ai";
+import { inngest } from "./client";
 
 export const blocking = inngest.createFunction(
   {
@@ -8,14 +10,45 @@ export const blocking = inngest.createFunction(
     triggers: [{ event: "demo/blocking" }],
   },
   async ({ event, step }) => {
-    const text = await step.run("generate-text", async () => {
-      const response = await generateText({
-        model: google("gemini-3.5-flash"),
-        prompt: "Write a short story about a cat",
-      });
-      return response.text;
+    const { prompt } = event.data as { prompt: string };
+
+    const urls = await step.run("scrape-urls", async () => {
+      return prompt.match(URL_REGEX) ?? [];
     });
 
-    return { message: `hello ${event.data.email}`, text };
+    const scrapedContent = await step.run("scrape-content", async () => {
+      const content = await Promise.all(
+        urls
+          .filter((url): url is string => typeof url === "string")
+          .map(async (url) => {
+            const response = await firecrawl.scrape(url, {
+              formats: ["markdown"],
+            });
+            return response.markdown;
+          }),
+      );
+      return content;
+    });
+    const finalPrompt = `
+      You are a helpful assistant that can answer questions and help with tasks.
+      You are given a prompt and a list of URLs.
+      You need to scrape the content of the URLs and use the content to answer the prompt.
+      The prompt is: ${prompt}
+      The URLs are: ${urls.join(", ")}
+      The scraped content is: ${scrapedContent.join("\n")}
+      `;
+    const content = await step.run("generate-content", async () => {
+      const response = await generateText({
+        model: google("gemini-3.5-flash"),
+        prompt: finalPrompt,
+      });
+
+      return {
+        message: `hello ${event.data}`,
+        text: response.text,
+      };
+    });
+
+    return { urls, scrapedContent, content };
   },
 );
