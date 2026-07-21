e750c0d679bd01ae7c3c8256200eb595dca28e9c
Akbarzadeh.ali <Alireza.Akbarzadeh.dev@gmail.com>
2026-07-21 15:04:55 +0330

    Add Clerk authentication and UI components, update dependencies, and introduce new proxy and prompt files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Mono, Inter } from "next/font/google";
 import ThemeProvider from "@/components/theme-provider";
+import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 
 import "./globals.css";
+import { dark } from "@clerk/themes";
 
 
 const ibmPlexMono = IBM_Plex_Mono({
@@ -27,14 +29,31 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <ClerkProvider appearance={{
+      theme: dark 
+    }}>
+      <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.variable} ${ibmPlexMono.variable} antialiased`}
       >
+        <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <Show when="signed-out">
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </Show>
+            <Show when="signed-in">
+              <UserButton />
+            </Show>
+          </header>
         <ThemeProvider defaultTheme="dark" attribute="class" enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
